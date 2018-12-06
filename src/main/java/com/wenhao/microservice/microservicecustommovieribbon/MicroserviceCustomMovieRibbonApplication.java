ebdcbe2f0e860b809591b0f39f6d72702909f22f
wenhao176 <921790476@qq.com>
2018-12-06 16:32:16 +0800

    没有注册到 eureka服务上时 使用ribbon

--- src/main/java/com/wenhao/microservice/microservicecustommovieribbon/MicroserviceCustomMovieRibbonApplication.java
+++ src/main/java/com/wenhao/microservice/microservicecustommovieribbon/MicroserviceCustomMovieRibbonApplication.java
@@ -2,19 +2,17 @@ package com.wenhao.microservice.microservicecustommovieribbon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class MicroserviceCustomMovieRibbonApplication {
 
 
     @Bean
     @LoadBalanced
-    public RestTemplate restTemplate(){
+    public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 
